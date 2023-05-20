b356f13e1df4dfd705b4a60c08e58c68119a1606
ChessWizard <chesswizard2002@gmail.com>
2023-05-20 17:15:53 +0300

    Password Security Status Added

---
+++ PasswordManager.Core/Constants/PasswordSecurityLength.cs
@@ -0,0 +1,13 @@
+﻿namespace PasswordManager.Core.Constants
+{
+    public static class PasswordSecurityLength
+    {
+        public const int DefaultLevelPassword = 4;
+
+        public const int LowLevelPassword = 10;
+
+        public const int MidLevelPassword = 20;
+
+        public const int HighLevelPassword = 30;
+    }
+}
---
+++ PasswordManager.Core/Constants/PasswordSecurityMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace PasswordManager.Core.Constants
+{
+    public static class PasswordSecurityMessage
+    {
+        public const string NotValidPasswordCondition = "Lütfen en az bir tane şifre oluşturma koşulu seçiniz.";
+
+        public const string LowLevelPassword = "bg-danger";
+
+        public const string MidLevelPassword = "bg-warning";
+
+        public const string HighLevelPassword = "bg-success";
+    }
+}
