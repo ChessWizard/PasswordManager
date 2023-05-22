1d10547d8df9c16250d22b332838d51d37b98e18
ChessWizard <chesswizard2002@gmail.com>
2023-05-23 00:45:25 +0300

    Delete Password Operations Added

--- PasswordManager.Core/Constants/ToasterResponseMessages.cs
+++ PasswordManager.Core/Constants/ToasterResponseMessages.cs
@@ -5,5 +5,9 @@
         public const string CopiedSuccessfully = "Kopyalama işlemi başarılı!";
 
         public const string PasswordAlreadySaved = "Bu şifre zaten favorilerde bulunuyor!";
+
+        public const string FavoritePasswordDeleted = "Şifre silindi!";
+
+        public const string NotFoundFavoritePasswordForDelete = "Silinmek üzere herhangi kayıtlı bir şifre bulunamadı!";
     }
 }
