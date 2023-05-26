48bbb4146de0280bfb35091f9fa67bd61a676aa8
ChessWizard <chesswizard2002@gmail.com>
2023-05-26 17:14:26 +0300

    Storing Password Actions Added

--- PasswordManager.Core/Constants/ToasterResponseMessages.cs
+++ PasswordManager.Core/Constants/ToasterResponseMessages.cs
@@ -2,12 +2,21 @@
 {
     public static class ToasterResponseMessages
     {
-        public const string CopiedSuccessfully = "Kopyalama işlemi başarılı!";
+        public struct Success
+        {
+            public const string CopiedSuccessfully = "Kopyalama işlemi başarılı!";
 
-        public const string PasswordAlreadySaved = "Bu şifre zaten favorilerde bulunuyor!";
+            public const string FavoritePasswordDeleted = "Şifre silindi!";
+        }
 
-        public const string FavoritePasswordDeleted = "Şifre silindi!";
+        public struct Warning
+        {
+            public const string PasswordAlreadySaved = "Bu şifre zaten favorilerde bulunuyor!";
+        }
 
-        public const string NotFoundFavoritePasswordForDelete = "Silinmek üzere herhangi kayıtlı bir şifre bulunamadı!";
+        public struct Error
+        {
+            public const string NotFoundFavoritePasswordForDelete = "Silinmek üzere herhangi kayıtlı bir şifre bulunamadı!";
+        }
     }
 }
--- PasswordManager.Core/Program.cs
+++ PasswordManager.Core/Program.cs
@@ -1,7 +1,10 @@
+using Blazored.LocalStorage;
 using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PasswordManager.Core;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -9,6 +12,19 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 // Toaster
 builder.Services.AddBlazoredToast();
+
+// Local Storage
+builder.Services.AddBlazoredLocalStorage(config =>
+{
+    config.JsonSerializerOptions.DictionaryKeyPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    config.JsonSerializerOptions.IgnoreReadOnlyProperties = true;
+    config.JsonSerializerOptions.PropertyNameCaseInsensitive = true;
+    config.JsonSerializerOptions.PropertyNamingPolicy = JsonNamingPolicy.CamelCase;
+    config.JsonSerializerOptions.ReadCommentHandling = JsonCommentHandling.Skip;
+    config.JsonSerializerOptions.WriteIndented = false;
+});
+
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
